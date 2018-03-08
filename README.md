648df9d34c695857d5e4a3ad18991c8cf601f6a6
马斯珣 <masixun@baijiahulian.com>
2018-03-08 11:53:10 +0800

    up readme

--- README.md
+++ README.md
@@ -4,3 +4,30 @@
 
 
 #### 一开始采用红黑树实现，但发现有序数组查找效率和红黑树效率一样，甚至略高，替换为有序数组，节点加入了权重，在增删节点时必须重置节点数组，红黑树插入效率也低于数据，最终替换为有序数组，只进行一次数组排序。
+
+
+安装｜install
+===
+
+	go get github.com/masixun71/Chashringgo
+
+Example
+===
+
+
+```
+	nodeWeight := make(map[string]int)
+	nodeWeight[node1] = 1
+	nodeWeight[node2] = 1
+	nodeWeight[node3] = 1
+	vitualSpots := 1
+
+	//hash := NewChashring(vitualSpots)
+	hash := NewChashringWithHashAlgorithm(vitualSpots, &FNV1_32_HASH{})
+
+	hash.AddNodes(nodeWeight)
+	hash.AddNode(node3, 1)
+	hash.RemoveNode(node3)
+	hash.UpdateNode(node3, 1)
+	nodeIndex := hash.GetNode("1")
+```
--- chashring_test.go
+++ chashring_test.go
@@ -1,7 +1,6 @@
 package Chashringgo
 
 import (
-	//	"fmt"
 	"testing"
 )
 
@@ -34,66 +33,43 @@ func TestHash(t *testing.T) {
 	nodeWeight[node3] = 1
 	vitualSpots := 1
 
-	hash := NewChashring(vitualSpots)
+	//hash := NewChashring(vitualSpots)
+	hash := NewChashringWithHashAlgorithm(vitualSpots, &FNV1_32_HASH{})
+
+
 	hash.AddNodes(nodeWeight)
 
-	//var ss []rbtree.Item
-	//hash.nodes.Ascend(hash.nodes.Min(), func(i rbtree.Item) bool {
-	//	ss = append(ss, i)
-	//	return true
-	//})
-
-
-	//node1Count := 0
-	//node2Count := 0
-	//node3Count := 0
-	//for i :=1;i <=10; i++ {
-	//	tKey := hash.GetNode(strconv.Itoa(i))
-	//	t.Log(tKey)
-	//	if tKey == node1 {
-	//		node1Count += 1
-	//	}
-	//	if tKey == node2 {
-	//		node2Count += 1
-	//
-	//	}
-	//	if tKey == node3 {
-	//		node3Count += 1
-	//
-	//	}
-	//}
-	//t.Logf("nodes node1:%v, node2:%v, node3:%v", node1Count, node2Count, node3Count)
-	//t.Log(hash)
 	if hash.GetNode("1") != node2 {
-		t.Fatalf("expetcd %v got %v", node2, hash.GetNode("1"))
+		t.Fatal("get node is error")
 	}
 	if hash.GetNode("2") != node1 {
-		t.Fatalf("expetcd %v got %v", node1, hash.GetNode("2"))
+		t.Fatal("get node is error")
 	}
 	if hash.GetNode("4") != node3 {
-		t.Fatalf("expetcd %v got %v", node3, hash.GetNode("3"))
+		t.Fatal("get node is error")
 	}
 
 	hash.RemoveNode(node3)
+	hash.UpdateNode(node3, 1)
 	if hash.GetNode("1") != node2 {
-		t.Fatalf("expetcd %v got %v", node2, hash.GetNode("1"))
+		t.Fatal("get node is error")
 	}
 	if hash.GetNode("2") != node1 {
-		t.Fatalf("expetcd %v got %v", node1, hash.GetNode("2"))
+		t.Fatal("get node is error")
 	}
 	if hash.GetNode("4") != node1 {
-		t.Fatalf("expetcd %v got %v", node1, hash.GetNode("3"))
+		t.Fatal("get node is error")
 	}
 
 	hash.AddNode(node3, 1)
 	if hash.GetNode("1") != node2 {
-		t.Fatalf("expetcd %v got %v", node2, hash.GetNode("1"))
+		t.Fatal("get node is error")
 	}
 	if hash.GetNode("2") != node1 {
-		t.Fatalf("expetcd %v got %v", node1, hash.GetNode("2"))
+		t.Fatal("get node is error")
 	}
 	if hash.GetNode("4") != node3 {
-		t.Fatalf("expetcd %v got %v", node3, hash.GetNode("3"))
+		t.Fatal("get node is error")
 	}
 
 }
